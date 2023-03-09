0dfe012bf05bf259ef47b735fb93c8fc8bb47a1c
Philippe Auriou <philippe.auriou@showroomprive.net>
2023-03-09 12:07:33 +0100

    clean ps1

--- kafka/clean.ps1
+++ kafka/clean.ps1
@@ -3,3 +3,7 @@ docker network prune -f
 docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
 #docker-compose up 
+#docker image prune -a --filter "until=24h"
+
+
+#docker system prune -a
